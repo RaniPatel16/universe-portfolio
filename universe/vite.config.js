36b0b636a3b1ab455d658694490fbefa70f519ab
Rani Patel <ranip161206@gmail.com>
2026-07-07 22:12:32 +0530

    Remove base path from Vite configuration
    Removed base path configuration from Vite config.

--- universe/vite.config.js
+++ universe/vite.config.js
@@ -1,11 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/universe-portfolio/',
   plugins: [react()],
   build: {
     target: 'esnext',
     chunkSizeWarningLimit: 2000,
   },
-});
+})
